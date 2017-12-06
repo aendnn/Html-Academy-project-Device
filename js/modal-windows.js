74d304f9a8dc6d36d5d282a6210a13a34e656312
Anna <4n4katana@gmail.com>
2017-12-06 23:06:14 +0200

    js для открытия/закрытия фидбэка

--- js/modal-windows.js
+++ js/modal-windows.js
@@ -1,1 +1,15 @@
 var link = document.querySelector(".write-us");
+var feedback = document.querySelector(".modal-feedback");
+var close = document.querySelector(".modal-close");
+
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  feedback.classList.add("modal-show");
+} );
+
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  feedback.classList.remove("modal-show");
+} );
